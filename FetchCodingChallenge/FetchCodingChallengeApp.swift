b273f699709674aceccdfb919a4828895a5c683e
noviecat <gyang317@gmail.com>
2023-06-05 12:09:26 -0500

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Mary Emerald on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
